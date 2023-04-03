e3fd4d7fac15036cdeb06dbc52a9302a54388867
Ferdinand Malcher <ferdinand@malcher.media>
2023-04-03 11:36:39 +0200

    Dashboard und Bücher

--- book-rating/src/main.ts
+++ book-rating/src/main.ts
@@ -5,34 +5,3 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-
-
-
-
-  /////////
-
-  const foo = function (arg) {
-    return arg + 1;
-  }
-
-  const foo1 =  arg => arg + 1;
-
-
-
-
-class Customer {
-
-  private id = 4;
-
-  fooBar() {
-
-    setTimeout(() => {
-      console.log('Die ID ist', this.id);
-    }, 2000);
-  }
-}
-
-
-const myCustomer = new Customer();
-myCustomer.fooBar()
