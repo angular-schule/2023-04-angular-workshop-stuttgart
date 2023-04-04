c1fb120443092f3594e833d19d22a3ceebfd2f8f
Ferdinand Malcher <ferdinand@malcher.media>
2023-04-04 21:04:02 +0200

    RxJS-Playground hinzugefügt

---
+++ rxjs-playground/src/app/exercises/game-score/game-score.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { Subject, ReplaySubject, scan, reduce } from 'rxjs';
+
+@Component({
+  selector: 'rxw-game-score',
+  templateUrl: './game-score.component.html',
+})
+export class GameScoreComponent {
+
+  logStream$ = new ReplaySubject<string | number>();
+  score$ = new Subject<number>();
+
+  currentScore = 0;
+
+  constructor() {
+    /**
+     * Wir entwickeln ein spannendes Browser-Spiel!
+     * Jetzt fehlt nur noch der Code, um den Punktestand zu ermitteln ...
+     */
+
+    /******************************/
+
+
+    /******************************/
+
+    this.score$.subscribe({
+      next: e => this.logStream$.next(e),
+      complete: () => this.logStream$.next('✅ COMPLETE')
+    });
+  }
+
+  addScore(amount: number) {
+    this.score$.next(amount);
+  }
+
+}
