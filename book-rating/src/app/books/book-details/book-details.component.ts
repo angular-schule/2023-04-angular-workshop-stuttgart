52e59f85f7e5de6534c6f8dd3b186e10d17f1aeb
Ferdinand Malcher <ferdinand@malcher.media>
2023-04-05 12:43:42 +0200

    Detailseite: Bücher laden

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BookStoreService } from '../shared/book-store.service';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-book-details',
@@ -7,4 +10,19 @@ import { Component } from '@angular/core';
 })
 export class BookDetailsComponent {
 
+  book?: Book;
+
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) {
+    // Synchroner Weg / PULL
+    // const isbn = this.route.snapshot.paramMap.get('isbn'); // path: 'books/:isbn'
+    // console.log(isbn);
+
+    // Asynchroner Weg / PUSH
+    this.route.paramMap.subscribe(params => {
+      const isbn = params.get('isbn')!;
+      this.bs.getSingle(isbn).subscribe(book => {
+        this.book = book;
+      });
+    });
+  }
 }
